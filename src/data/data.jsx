0b4c2641f4cb334c636dbf5073362b527ef3b6b0
eduardojulian <eduardojulian@tabasco.gob.mx>
2025-06-20 15:31:59 -0600

    card terminados usando tailwind

---
+++ src/data/data.jsx
@@ -0,0 +1,13 @@
+export const dataProyectos =[
+    {
+        name:"Proyecto #1",
+        detalle:"descripcion",
+        img: "https://cdnb.artstation.com/p/assets/images/images/025/405/301/large/alexandra-demchuk-alex.jpg?1585687220"
+    },
+
+    {
+        name:"Proyecto #2",
+        detalle:"descripcion",
+        img: "https://assets.mycast.io/actor_images/actor-alex-totally-spies-496873_large.jpg?1659044711"
+    }
+]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import {useThemeStore} from "../store/useThemeStore";
+import {dataProyectos} from "../data/data";
+import { CardMenu } from "../components/HomeComponents/CardMenu";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+export const Home = () => {
+    const {theme, toggleTheme} = useThemeStore();
+    
+
+    return (
+        <main className="bg-primary h-screen dark:bg-primary-dark flex flex-col items-center justify-center overflow-hidden">
+            <section className="rounded-lg min-w-[450px] px-3">
+                <h1 className="text-black dark:text-white text-4xl font-bold mb-4 flex items-center gap-4">PROYECTOS 
+                    <Icon icon="skill-icons:tailwindcss-dark" width="40" height="40" /></h1>
+                {
+                    dataProyectos.map((item,index)=>{
+                        return(
+                            <CardMenu key={index} item={item} index={index} />
+                        )
+                    })
+                }
+            </section>
+        
+        <button className="p-2 mt-3 rounded-lg bg-white dark:bg-black text-black dark:text-white font-semibold" onClick={toggleTheme}>
+            {theme === "light" ? "Modo claro":"Modo oscuro"}
+        </button>
+        </main>
+    )
+}
